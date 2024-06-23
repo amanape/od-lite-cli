f67e00ef16d542a88c9b043d394b22d121647d13
amanape <83104063+amanape@users.noreply.github.com>
2024-06-23 14:52:14 +0300

    Update flow

--- agent.ts
+++ agent.ts
@@ -1,18 +1,21 @@
-import { Agent } from "od-lite";
+import { Agent, Topic } from "od-lite";
+import type { Action } from "od-lite/dist/types/actions";
 import Together from "together-ai";
 import { EXAMPLES, MINIMAL_SYSTEM_PREFIX, SYSTEM_SUFFIX } from "./prompt";
+import OpenAI from "openai";
+import type { Message } from "od-lite/dist/types/messages";
 
 const systemPrompt = `${MINIMAL_SYSTEM_PREFIX}\n\n${SYSTEM_SUFFIX}`;
 const example = `Here is an example of how you can interact with the environment for task solving:\n${EXAMPLES}\n\nNOW, LET'S START!`
 
-const parse = (response?: string) => {
+const parse = (response: string | undefined | null) => {
   const regex = /<execute_command>(.*?)<\/execute_command>/;
   const match = response?.match(regex);
 
   return match?.[1];
 }
 
-class TogetherAgent implements Agent {
+export class TogetherAgent implements Agent {
   private readonly together: Together;
 
   constructor() {
@@ -21,7 +24,7 @@ class TogetherAgent implements Agent {
     });
   }
 
-  public async query(message: string): Promise<{ command: string; }> {
+  public async query(message: string): Promise<Action | Message> {
     const response = await this.together.chat.completions.create({
       model: "Qwen/Qwen2-72B-Instruct",
       messages: [
@@ -31,9 +34,40 @@ class TogetherAgent implements Agent {
       ],
     });
 
-    const command = parse(response.choices?.[0].message?.content);
-    return { command: command };
+    const content = response.choices?.[0].message?.content;
+    const command = parse(content);
+
+    if (command) return { type: Topic.ACTION, data: { command: command } };
+    return { type: Topic.MESSAGE, data: { message: content } };
   }
 }
 
-export default TogetherAgent;
+export class OpenAIAgent implements Agent {
+  private readonly openai: OpenAI;
+  private readonly messages: { role: "user" | "system"; content: string; }[] = [
+    { role: "system", content: systemPrompt },
+    { role: "user", content: example },
+  ];
+
+  constructor() {
+    this.openai = new OpenAI({
+      apiKey: Bun.env.OPENAI_API_KEY,
+    });
+  }
+
+  public async query(message: string): Promise<Action | Message> {
+    const response = await this.openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        ...this.messages,
+        { role: "user", content: message },
+      ],
+    });
+
+    const content = response.choices?.[0].message?.content;
+    const command = parse(content);
+
+    if (command) return { type: Topic.ACTION, data: { command: command } };
+    return { type: Topic.MESSAGE, data: { role: "ai", message: content } };
+  }
+}
--- index.ts
+++ index.ts
@@ -1,23 +1,88 @@
-import { ObservationFactory, type Runtime, type TerminalManager } from "od-lite";
-import TogetherAgent from "./agent";
+import { ObservationFactory, type Runtime, type TerminalManager, RxPubSub, Topic } from "od-lite";
+import { OpenAIAgent } from "./agent";
 import TM from "./tm";
 import type { Action } from "od-lite/dist/types/actions";
 import type { Observation } from "od-lite/dist/types/observations";
+import chalk from "chalk";
+
+const pubsub = new RxPubSub();
 
 class BasicRuntime implements Runtime {
   constructor(private readonly terminalManager: TerminalManager) { }
 
   public async handle(action: Action): Promise<Observation> {
-    const output = await this.terminalManager.write(action.command);
-    return ObservationFactory.fromTerminalOutput(action.command, output);
+    const output = await this.terminalManager.write(action.data.command);
+    return ObservationFactory.fromTerminalOutput(action.data.command, output);
   }
 }
 
 const runtime = new BasicRuntime(new TM());
-const agent = new TogetherAgent();
+const openAIAgent = new OpenAIAgent();
 
-const action = await agent.query("Check all active network connections.");
-console.log('Action:', action);
+const message = pubsub.subscribe(Topic.MESSAGE);
+const observation = pubsub.subscribe(Topic.OBSERVATION);
+const action = pubsub.subscribe(Topic.ACTION);
 
-const observation = await runtime.handle(action);
-console.log('Observation:', observation);
+action.subscribe({
+  next: async (payload) => {
+    const observation = await runtime.handle(payload);
+    pubsub.publish(observation);
+  },
+});
+
+action.subscribe({
+  next: async (payload) => {
+    console.log(chalk.yellow('[ACTION]'), chalk.blue(payload.data.command));
+  }
+});
+
+message.subscribe({
+  next: async (payload) => {
+    if (payload.data.role === 'user') {
+      const action = await openAIAgent.query(payload.data.message);
+      pubsub.publish(action);
+    }
+  },
+});
+
+message.subscribe({
+  next: async (payload) => {
+    console.log(chalk.gray(JSON.stringify(payload, null, 2)));
+    
+    if (payload.data.role === 'ai') {
+      console.log(`${chalk.blue('OpenDevin')}: `, payload.data.message);
+    } else {
+      console.log(chalk.gray('[LOADING]'), 'Processing your request...');
+    }
+  }
+});
+
+observation.subscribe({
+  next: async (payload) => {
+    const action = await openAIAgent.query(payload.data.output);
+
+    if (action.type === Topic.MESSAGE) {
+      pubsub.publish(action);
+    }
+
+    // pubsub.publish(action);
+  }
+});
+
+observation.subscribe({
+  next: (payload) => {
+    console.log(chalk.yellow('[OBSERVATION]'), payload.data.output);
+  }
+});
+
+import * as readline from 'readline';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question(`${chalk.blue('OpenDevin')}: How can I help you today?\n${chalk.green('You')}: `, async (answer) => {
+  pubsub.publish({ type: Topic.MESSAGE, data: { role: "user", message: answer } });
+  rl.close();
+});
