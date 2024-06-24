1eb7cd23a2fd643131632e229d6a0791e494d6b9
amanape <83104063+amanape@users.noreply.github.com>
2024-06-24 17:49:12 +0300

    Remove verbose logs

--- src/event-handler.ts
+++ src/event-handler.ts
@@ -4,8 +4,6 @@ import { isCommandObservation, type Observation } from "./types/observations";
 
 export class EventHandler {
   static async handleAction(action: Action) {
-    console.log(chalk.gray(JSON.stringify(action, null, 2)));
-
     if (isCommandAction(action)) {
       console.log('\n' + chalk.yellow('[ACTION]'), '> ' + chalk.blue(action.data.command));
     } else {
@@ -14,8 +12,6 @@ export class EventHandler {
   }
 
   static async handleObservation(observation: Observation) {
-    console.log(chalk.gray(JSON.stringify(observation, null, 2)));
-
     if (isCommandObservation(observation)) {
       const stripped = observation.data.output.endsWith('\n') ? observation.data.output.slice(0, -1) : observation.data.output;
       console.log(`${chalk.yellow('[OBSERVATION]')}${stripped ? '\n' + stripped : ' ' + chalk.gray('EMPTY')}`);
